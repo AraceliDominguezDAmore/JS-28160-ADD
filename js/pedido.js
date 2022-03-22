146f3d838150a15ee148f46bd2b287b24d2f6022
AraceliDominguezDAmore <dominguezaraceli2701@gmail.com>
2022-03-22 20:22:06 -0300

    FETCH

--- js/pedido.js
+++ js/pedido.js
@@ -9,31 +9,28 @@ cargarEventos();
 
 function cargarEventos(){
 
-    //Se ejecuta cuando se presionar agregar carrito
-    productos.addEventListener('click', (e)=>{
-        carro.comprarProducto(e)
-        const Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 2000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.addEventListener('mouseenter', Swal.stopTimer)
-              toast.addEventListener('mouseleave', Swal.resumeTimer)
-            }
-          })
-          
-          Toast.fire({
-            icon: 'success',
-            title: 'Producto agregado correctamente'
-          })});
+  //Se ejecuta cuando se presionar comprar
+  productos.addEventListener('click', (e)=>{
+    carro.comprarProducto(e)
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+      Toast.fire({
+        icon: 'success',
+        title: 'Producto agregado correctamente'
+      })
+  });
 
     //Cuando se elimina productos del carrito
     carrito.addEventListener('click', (e)=>{carro.eliminarProducto(e)});
-
-    //Al vaciar carrito
-    vaciarCarritoBtn.addEventListener('click', (e)=>{carro.vaciarCarrito(e)});
 
     //Al cargar documento se muestra lo almacenado en LS
     document.addEventListener('DOMContentLoaded', carro.leerLocalStorage());
